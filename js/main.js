0816a27331bb70bfe6b251dec1dc9560fe6aa45c
Ivan Dovhopol <ivandovhopol@gmail.com>
2022-07-22 19:15:14 +0300

    test JS

---
+++ js/main.js
@@ -0,0 +1,71 @@
+'use strict';
+const yearOfBirth = 2002;
+console.log(yearOfBirth);
+
+const yearToday = 20;
+console.log(yearToday);
+
+const dateNow = 22.07;
+console.log(dateNow);
+
+const price = 10;
+console.log(price);
+
+let age;
+console.log(age);
+
+age = 14;
+console.log(age);
+
+age = 15;
+console.log(age);
+
+age = 18;
+console.log(age);
+
+// КОНСТАНТЫ
+const COLOR_TEAL = '#009688';
+
+// ПРИМИТИВНЫЕ ТИПЫ
+// Number
+const points = 19.99;
+console.log(points);
+
+//String
+const username = 'Ivan';
+console.log(username);
+
+const description = 'Wellcome!';
+console.log(description);
+
+//Boolean
+const isModalOpen = false;
+const theTableClear = false;
+const isLampOn = true;
+console.log(isModalOpen, isLampOn);
+
+// null
+let selectedProduct = null;
+console.log(selectedProduct);
+
+// undefined
+let surname;
+console.log(typeof surname);
+
+// ВЗАЕМОДЕЙСТВИЕ С ПОЛЬЗОВАТЕЛЕМ
+// Вывод данных
+const message = 'hello world!';
+console.log(message);
+
+const fullname = 'Ivan Dovhopol';
+console.log('name:', fullname);
+
+const prewiew = 'Добро пожаловать на страницу!';
+alert(prewiew);
+
+// Получение данных
+const isPerson = confirm('Подтвердите действие на странице');
+console.log(isPerson);
+
+const userLogin = prompt('Напишите ваше имя');
+console.log(userLogin);
