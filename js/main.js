312a6df557bd4c328e5247220a38001c529013da
Ivan Dovhopol <ivandovhopol@gmail.com>
2022-07-27 00:32:28 +0300

    checkToday

--- js/main.js
+++ js/main.js
@@ -314,3 +314,43 @@
 
 // console.log('Вы ввели логин:', userLogin);
 // console.log('Вы ввели пароль:', userPassword);
+
+// Цыклы
+// for (let i = 0; i <= 20; i += 5) {
+//   console.log(i);
+// }
+
+// const target = 3;
+// let sum = 0;
+
+// for (let i = 0; i <= target; i += 1) {
+//     sum += i;
+// }
+
+// console.log(sum);
+
+// const max = 10;
+
+// for (let i = 0; i < max; i += 1) {
+//     console.log(`${max} % ${i}`, max % i)
+// }
+
+// for (let i = 0; i <= 5; i += 1) {
+//   console.log(i);
+//   if (i === 3) {
+//     console.log('log up');
+//     break;
+//   }
+// }
+
+// console.log('log down');
+
+const number = 10;
+
+for (let i = 0; i < number; i += 1) {
+  if (i % 2 === 0) {
+    continue;
+  }
+
+  console.log('Нечетное i: ', i); // 1, 3, 5, 7, 9
+}
