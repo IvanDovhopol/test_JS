53e612e4a6f97837ca7385a9959a44e6a07f9e6e
Ivan Dovhopol <ivandovhopol@gmail.com>
2022-09-15 03:38:53 +0300

    create my task

--- js/dom.js
+++ js/dom.js
@@ -342,3 +342,103 @@
 // inputRef.addEventListener('input', event => {
 //   outputRef.textContent = event.currentTarget.value;
 // });
+
+//  =====================NX================== <|> Подключение библиотеки <|>
+
+// const res = _.add(333, 333);
+// console.log(res);
+
+// document.addEventListener('scroll', () => {
+//   console.log('скролл без лимита');
+// });
+
+// // Throttle
+
+// document.addEventListener(
+//   'scroll',
+//   _.throttle(() => {
+//     console.log('скролл через - throttle');
+//   }, 300),
+// );
+
+// // debounce
+
+// document.addEventListener(
+//   'scroll',
+//   _.debounce(() => {
+//     console.log('скролл через - debounce');
+//   }, 100),
+// );
+
+// // Режимы метода debounce
+
+// document.addEventListener(
+//   'scroll',
+//   _.debounce(
+//     () => {
+//       console.log('скролл через - debounce#22222');
+//     },
+//     100,
+//     {
+//       leading: true,
+//       trailing: false,
+//     },
+//   ),
+// );
+
+//  =====================NX================== <|> Отложенная загрузка <|>
+
+// Библиотека lazysizes
+
+const btnRef = document.querySelector('.btn');
+const btnYesRef = document.querySelector('.btn.left');
+
+btnRef.addEventListener('mouseover', randomPositionBtnOnHover);
+btnYesRef.addEventListener('click', onOpenModalWindow);
+
+let instance = '';
+
+function randomPositionBtnOnHover(e) {
+  const { target: btn } = e;
+
+  btnRef.style.top = `${Math.floor(
+    Math.random() * (window.innerHeight - btn.clientHeight),
+  )}px`;
+
+  btnRef.style.left = `${Math.floor(
+    Math.random() * (window.innerWidth - btn.clientWidth),
+  )}px`;
+
+  randomColor();
+}
+
+function onOpenModalWindow(e) {
+  instance = basicLightbox.create(`
+    <div class="modal">
+        <p style="color: #fff">
+            Спасибо за ваш отзыв	
+&#128077;
+        </p>
+    </div>
+`);
+
+  instance.show();
+
+  onCloseModalWindowKey(e);
+
+  window.addEventListener('keydown', onCloseModalWindowKey);
+}
+
+function randomColor() {
+  const randomHexColor = `#${Math.floor(Math.random() * 16777215).toString(
+    16,
+  )}`;
+
+  btnRef.style.backgroundColor = randomHexColor;
+}
+
+function onCloseModalWindowKey(e) {
+  if (e.code === 'Escape') {
+    instance.close();
+  }
+}
